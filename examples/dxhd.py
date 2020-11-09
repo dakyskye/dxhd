922222f1e1a021ee463174c4c065707aa24f7ab4
dakyskye <kanteladzelasha339@gmail.com>
2020-11-09 21:48:32 +0400

    update python example

--- examples/dxhd.py
+++ examples/dxhd.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
-# super + Return
 import os
+
+# super + Return
 os.popen('alacritty')
 
 #super + d
-import os
 os.popen('rofi -modi drun -show drun')
 
 #super+{_,shift}+{1-9,0}
-import os
 os.popen('zenity --no-wrap --info --text="PY i3-msg -t command {_,move container to} workspace {1-9,10}"')
 
 #super+ctrl+{_,shift}+{1-9,0}
-import os
 os.popen('zenity --no-wrap --info --text="PY i3-msg -t command {_,move container to} workspace {11-19,20}"')
