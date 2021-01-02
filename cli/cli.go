d877e6923432f7475aa4cfe1573bf3f39f5485a2
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-01-03 01:46:17 +0400

    end flag descriptions with full stop; added verbose flag

--- cli/cli.go
+++ cli/cli.go
@@ -20,6 +20,7 @@ type Options struct {
 	DryRun      *bool
 	Background  *bool
 	Interactive *bool
+	Verbose     *bool
 	Config      *string
 	Edit        *string
 }
@@ -51,15 +52,15 @@ func Init() (c CLI) {
 	c.app.UsageTemplate(usageTemplate)
 
 	c.app.HelpFlag.Short('h')
-	c.app.VersionFlag.Short('v')
 
-	c.Options.Kill = c.app.Flag("kill", "Kills all the running instances of dxhd").Short('k').Action(c.kill).Bool()
-	c.Options.Reload = c.app.Flag("reload", "Reloads all the running instances of dxhd").Short('r').Action(c.reload).Bool()
-	c.Options.DryRun = c.app.Flag("dry-run", "Does a dry run (prints parsed results and exits)").Short('d').Action(c.dryrun).Bool()
-	c.Options.Background = c.app.Flag("background", "Runs dxhd as a background process").Short('b').Action(c.background).Bool()
-	c.Options.Interactive = c.app.Flag("interactive", "Runs dxhd interactively").Short('i').Action(c.interactive).Bool()
-	c.Options.Config = c.app.Flag("config", "Parses the given config (defaults to dxhd.sh)").Short('c').Action(c.config).String()
-	c.Options.Config = c.app.Flag("edit", "Starts an editor on the given config file (defaults to dxhd.sh)").Short('e').Action(c.edit).String()
+	c.Options.Kill = c.app.Flag("kill", "Kills all the running instances of dxhd.").Short('k').Action(c.kill).Bool()
+	c.Options.Reload = c.app.Flag("reload", "Reloads all the running instances of dxhd.").Short('r').Action(c.reload).Bool()
+	c.Options.DryRun = c.app.Flag("dry-run", "Does a dry run (prints parsed results and exits).").Short('d').Action(c.dryrun).Bool()
+	c.Options.Background = c.app.Flag("background", "Runs dxhd as a background process.").Short('b').Action(c.background).Bool()
+	c.Options.Interactive = c.app.Flag("interactive", "Runs dxhd interactively.").Short('i').Action(c.interactive).Bool()
+	c.Options.Verbose = c.app.Flag("verbose", "Enables verbosity (logs everything).").Short('v').Action(c.verbose).Bool()
+	c.Options.Config = c.app.Flag("config", "Parses the given config (defaults to dxhd.sh).").Short('c').Action(c.config).String()
+	c.Options.Config = c.app.Flag("edit", "Starts an editor on the given config file (defaults to dxhd.sh).").Short('e').Action(c.edit).String()
 
 	logger.L().Debugln("initialised the app")
 
