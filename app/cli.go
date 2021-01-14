b78368b5267a7580bd03e84dcb523921a4496339
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-01-14 12:14:46 +0400

    allow initialising the app only once

--- app/cli.go
+++ app/cli.go
@@ -2,10 +2,11 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"sync"
 
 	"github.com/dakyskye/dxhd/config"
-
 	"github.com/dakyskye/dxhd/logger"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -36,44 +37,60 @@ BUGS
   Report a bug here if you find one - https://github.com/dakyskye/dxhd/issues
 `
 
+var (
+	app  App
+	once sync.Once
+)
+
 // Init initialises a new app.
-func Init() (a App, err error) {
-	configFile, err := config.GetConfigFile()
-	if err != nil {
-		return
-	}
+func Init() (App, error) {
+	var err error
 
-	a.execName, err = os.Executable()
-	if err != nil {
-		return
-	}
+	logger.L().Debugln("app initialisation requested")
+	once.Do(func() {
+		configFile, e := config.GetConfigFile()
+		if e != nil {
+			err = e
+			return
+		}
 
-	a.opts.edit = configFile
-	a.opts.config = configFile
+		err = config.CreateDefaultConfig()
+		if err != nil {
+			err = fmt.Errorf("error creating default config: %w", err)
+			return
+		}
 
-	a.ctx, a.cancel = context.WithCancel(context.Background())
+		app.execName, err = os.Executable()
+		if err != nil {
+			return
+		}
 
-	a.cli = kingpin.New("dxhd", "daky's X11 Hotkey Daemon")
+		app.opts.edit = configFile
+		app.opts.config = configFile
 
-	a.cli.Version(version)
-	a.cli.Author("Lasha Kanteladze <kanteladzelasha339@gmail.com> (https://github.com/dakyskye)")
-	a.cli.UsageTemplate(usageTemplate)
+		app.ctx, app.cancel = context.WithCancel(context.Background())
 
-	a.cli.HelpFlag.Short('h')
+		app.cli = kingpin.New("dxhd", "daky's X11 Hotkey Daemon")
 
-	a.cli.Flag("kill", "Kills all the running instances of dxhd.").Short('k').Action(a.kill).Bool()
-	a.cli.Flag("reload", "Reloads all the running instances of dxhd.").Short('r').Action(a.reload).Bool()
-	a.cli.Flag("dry-run", "Does a dry run (prints parsed results and exits).").Short('d').Action(a.dryrun).Bool()
-	a.cli.Flag("background", "Runs dxhd as a background process.").Short('b').Action(a.background).Bool()
-	a.cli.Flag("interactive", "Runs dxhd interactively.").Short('i').Action(a.interactive).Bool()
-	a.cli.Flag("verbose", "Enables verbosity (logs everything).").Short('v').Action(a.verbose).Bool()
-	a.cli.Flag("config", "Parses the given config (defaults to dxhd.sh).").Short('c').Default(configFile).StringVar(&a.opts.config)
-	a.cli.Flag("edit", "Starts an editor on the given config file (defaults to dxhd.sh).").Short('e').Default(configFile).Action(a.edit).StringVar(&a.opts.edit)
+		app.cli.Version(version)
+		app.cli.Author("Lasha Kanteladze <kanteladzelasha339@gmail.com> (https://github.com/dakyskye)")
+		app.cli.UsageTemplate(usageTemplate)
 
-	logger.L().WithField("file", configFile).Debug("set default file for -c and -e flags")
-	logger.L().Debug("initialised the app")
+		app.cli.HelpFlag.Short('h')
 
-	return
+		app.cli.Flag("kill", "Kills all the running instances of dxhd.").Short('k').Action(app.kill).Bool()
+		app.cli.Flag("reload", "Reloads all the running instances of dxhd.").Short('r').Action(app.reload).Bool()
+		app.cli.Flag("dry-run", "Does a dry run (prints parsed results and exits).").Short('d').Action(app.dryrun).Bool()
+		app.cli.Flag("background", "Runs dxhd as a background process.").Short('b').Action(app.background).Bool()
+		app.cli.Flag("interactive", "Runs dxhd interactively.").Short('i').Action(app.interactive).Bool()
+		app.cli.Flag("verbose", "Enables verbosity (logs everything).").Short('v').Action(app.verbose).Bool()
+		app.cli.Flag("config", "Parses the given config (defaults to dxhd.sh).").Short('c').Default(configFile).StringVar(&app.opts.config)
+		app.cli.Flag("edit", "Starts an editor on the given config file (defaults to dxhd.sh).").Short('e').Default(configFile).Action(app.edit).StringVar(&app.opts.edit)
+
+		logger.L().Debug("initialised the app")
+	})
+
+	return app, err
 }
 
 func (a *App) Parse() (err error) {
