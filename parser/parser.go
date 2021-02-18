c9652dcbeca8c779946fbd7983885665ec71be05
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-02-18 11:44:03 +0400

    modified new parser generator function and internal parser struct

--- parser/parser.go
+++ parser/parser.go
@@ -3,7 +3,7 @@ package parser
 import (
 	"bufio"
 	"errors"
-	"io"
+	"os"
 
 	"github.com/dakyskye/dxhd/logger"
 )
@@ -12,28 +12,48 @@ import (
 type Parser struct {
 	fileName    string
 	reader      *bufio.Reader
-	res         ParseResult
+	res         parser
 	finalResult ParseResult
 	finished    bool
 }
 
+type parser struct {
+	lineNumber int
+	line       string
+	isPrefix   bool
+	parseRes   ParseResult
+}
+
 // ParseResult is where our parser packs the parsed data.
-type ParseResult struct{}
+type ParseResult struct {
+	Shell string
+}
 
 var (
 	// ErrParsingNotFinished is returned collection of parser results is requested before it finishes the job.
 	ErrParsingNotFinished = errors.New("the parser has not finished the job yet")
 	// ErrParsingHasFinished is returned when re-parse is requested, instead Collect should be requested.
 	ErrParsingHasFinished = errors.New("the parser has already finished the job")
+	// ErrParserHasNoReader is likely to be returned when New is not used to initialise a parser.
+	ErrParserHasNoReader = errors.New("the parser does not have any data to parse")
 )
 
 // New returns a new parser for a given file.
-func New(file io.Reader, fileName string) Parser {
+func New(fileName string) (Parser, error) {
 	logger.L().WithField("file", fileName).Debugln("made a new parser")
-	return Parser{
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return Parser{}, err
+	}
+
+	p := Parser{
 		fileName: fileName,
 		reader:   bufio.NewReader(file),
 	}
+	err = file.Close()
+
+	return p, err
 }
 
 // Collect returns the parsed result.
