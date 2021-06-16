a0b5fccbee636939a8c40922086f7dcdc832ffc5
NotUnlikeTheWaves <hleistra@gmail.com>
2021-06-16 23:04:02 +0200

    Add basic lexer

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,83 @@
-use std::io::{self, BufRead};
+// # shift + print
+// flameshot printscreen
+
+// shift + {0-9}
+// set-brightness {0-9}
+
+// shift + -
+
 
 fn main() {
     println!("Hello, parser 2!");
 
-
-    // match io::stdin().read_line(&mut parse_line) {
-    //     Ok(_) => {}
-    //     Err(error) => {
-    //         println!("Error reading line: {}", error)
-    //     }
-    // }
-
-    // let stdin = io::stdin();
-    // let mut iterator = stdin.lock().lines();
-    // let parse_line = iterator.next().unwrap().unwrap();
-
     let args: Vec<String> = std::env::args().collect();
-    let output = args.join(" ");
-
-    // cargo run some arguments i want to pass
-
-    // # super + ctrl + {d, n}
-    // scw {"d","n"}
+    let output = args[1..].join(" ");
 
     println!("We'll parse: {:?}", output);
+
+    let tokens = tokenize(&output);
+    println!("Tried to Lex: {:?}", tokens);
 }
 
+
+#[derive(Debug, Clone)]
+enum Token {
+    RangeStart,
+    RangeSeparator,
+    Comma,
+    RangeEnd,
+    Plus,
+    Text(String),
+    Whitespace
+}
+
+
+fn tokenize(input: &String) -> Result<Vec<Token>, String> {
+    let mut result: Vec<Token> = Vec::new();
+    let mut it = input.chars().peekable();
+    let mut text = String::new();
+
+    fn push_text(text: &mut String, vec: &mut Vec<Token>) {
+        if text.len() != 0 {
+            vec.push(Token::Text(String::from(&*text)));
+            text.clear()
+        }
+    }
+
+    while let Some(&c) = it.peek() {
+        match c {
+            '{' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::RangeStart)
+            }
+            '}' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::RangeEnd)
+            }
+            '+' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::Plus)
+            }
+            ' ' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::Whitespace)
+            }
+            ',' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::Comma)
+            }
+            '-' => {
+                push_text(&mut text, &mut result);
+                result.push(Token::RangeSeparator)
+            }
+            a => {
+                text.push(a);
+            }
+        }
+        it.next();
+    }
+    if text.len() != 0 {
+        result.push(Token::Text(String::from(text)));
+    }
+    return Ok(result)
+}
