65d878e20887a759f248c4f9f4ec8e5a43993139
NotUnlikeTheWaves <hleistra@gmail.com>
2021-06-17 23:29:09 +0200

    Start lexing, but actually

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::option;
-
 // # shift + print
 // flameshot printscreen
 
@@ -20,14 +18,17 @@ fn main() {
     let tokens = tokenize(&output);
     println!("Tried to tokenize: {:?}", tokens);
 
-    let lexed = split_till_plus(&tokens);
-    println!("Tried to tokenize: {:?}", lexed);
+    let split = split_till_plus(&tokens);
+    println!("Tried to tokenize: {:?}", split);
+
+    let lexed = lex(&tokens);
+    println!("Lexed:");
+    println!("{:?}", lexed);
 }
     
 fn split_till_plus(vec: &Vec<Token>) -> Result<Vec<Vec<Token>>, String> {
     let mut split: Vec<Vec<Token>> = Vec::new();
     let mut option_depth: i32 = 0;
-    let index: i32 = 0;
 
     for (idx, value) in vec.iter().enumerate() {
         match *value {
@@ -43,6 +44,8 @@ fn split_till_plus(vec: &Vec<Token>) -> Result<Vec<Vec<Token>>, String> {
                     }
                 }
             }
+            Token::RangeStart   => option_depth += 1,
+            Token::RangeEnd     => option_depth -= 1,
             _ => {}
         }
     }
@@ -53,24 +56,86 @@ fn split_till_plus(vec: &Vec<Token>) -> Result<Vec<Vec<Token>>, String> {
 fn lex(vec: &Vec<Token>) -> Result<Vec<LexNode>, String> {
     let mut result: Vec<LexNode> = Vec::new();
 
+    let split_result = split_till_plus(vec);
+
+    let parts = match split_result {
+        Ok(parts) => parts,
+        Err(error) => return Err(error)
+    };
+
+    for (_, part) in parts.iter().enumerate() {
+        match lex_part(part) {
+            Ok(node) => result.push(node),
+            Err(err) => return Err(err)
+        }
+    }
+
     Ok(result)
+}
+
+fn lex_part(vec: &Vec<Token>) -> Result<LexNode, String> {
+    if vec.len() == 0 {
+        return Err(String::from("lex_part: vector size is 0"));
+    }
+    let token = vec.first().unwrap();
+    match token {
+        Token::Text(str) => {
+            let content = if vec.len() > 1 {
+                    match lex_part(&vec[1..].to_vec()) {
+                    Ok(content) => Some([content].to_vec()),
+                    Err(err) => return Err(err)
+                }
+            } else {
+                None
+            };
+            return Ok(LexNode{
+                of_type: LexItem::Text(String::from(str)),
+                content: content
+            })
+        }
+        Token::RangeStart => {
+            // Wrong check! Go deep down til you find range end at same depth
+            // Use that instead
+            if *(vec.last().unwrap()) == Token::RangeEnd {
+                let closure = match lex_closure(&vec[1..vec.len()-1].to_vec()) {
+                    Ok(closure) => closure,
+                    Err(err) => return Err(err)
+                };
+                return Ok(LexNode{
+                    of_type: LexItem::Closure,
+                    content: Some([closure].to_vec())
+                })
+            } else {
+                return Err(String::from("No matching ending brace (}) to a starting brace ({)"))
+            }
+        }
+        _ => return Err(String::from("Bad expression!"))
+    }
+}
+
+fn lex_closure(vec: &Vec<Token>) -> Result<LexNode, String>
+{
+    fn split_till_comma(vec: &Vec<Token>) -> Vec<Vec<Token>> {
+        let mut split = Vec::new();
+        return split;
+    }
+    Err(String::from("Unknown error"))
 }
 
 #[derive(Debug, Clone)]
 struct LexNode {
-    content: Vec<LexNode>,
+    content: Option<Vec<LexNode>>,
     of_type: LexItem
 }
 
 #[derive(Debug, Clone)]
 enum LexItem {
-    Range,
-    Text,
-    Option
+    Text(String),
+    Closure
 }
 
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Token {
     RangeStart,
     RangeSeparator,
@@ -78,7 +143,7 @@ enum Token {
     RangeEnd,
     Plus,
     Text(String),
-    Whitespace
+    // Whitespace
 }
 
 
@@ -110,7 +175,7 @@ fn tokenize(input: &String) -> Vec<Token> {
             }
             ' ' => {
                 push_text(&mut text, &mut result);
-                result.push(Token::Whitespace)
+                // result.push(Token::Whitespace)
             }
             ',' => {
                 push_text(&mut text, &mut result);
