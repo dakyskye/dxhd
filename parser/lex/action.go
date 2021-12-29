3c3724f3b621952bdf27d81f8f64bf45b6a93de6
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-12-29 17:31:51 +0400

    implemented action lexer

--- parser/lex/action.go
+++ parser/lex/action.go
@@ -1,5 +1,42 @@
 package lex
 
+import "github.com/dakyskye/dxhd/parser/token"
+
 func lexAction(l *lexer) stateFn {
-	return nil
+	for {
+		ch := l.next()
+		if ch == '\n' {
+			continue
+		}
+		if ch == '#' && l.overlook() == '\n' && l.peek() != '#' {
+			l.pos -= l.width
+			l.emit(token.ACTION)
+			return lexKeybinding
+		}
+		if ch == '{' && l.overlook() != '\\' {
+			if err := lexActionVariantGroup(l); err != nil {
+				l.error(err)
+				return nil
+			}
+		}
+		if ch == '}' && l.overlook() != '\\' {
+			l.error(errExtraClosingMeta)
+			return nil
+		}
+	}
+}
+
+func lexActionVariantGroup(l *lexer) error {
+	for {
+		ch := l.next()
+		if ch == '\n' {
+			return errUnclosedVariantGroup
+		}
+		if ch == '{' && l.overlook() != '\\' {
+			return errExtraOpeningMeta
+		}
+		if ch == '}' && l.overlook() != '\\' {
+			return nil
+		}
+	}
 }
---
+++ parser/lex/action_test.go
@@ -0,0 +1,42 @@
+package lex
+
+import (
+	"github.com/dakyskye/dxhd/parser/token"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+const lexerInputAction = `#!/bin/bash
+
+FOO=bar
+TST=bsd
+## good comment
+ALL="${FOO} and ${TST}"
+
+# super + {a, b, c}
+echo "{$\{FOO\}, $\{TST\}, $\{ALL\}}"
+echo {a, b, c}
+## a comment
+echo foo
+
+# super + c`
+
+const actionMatch = `echo "{$\{FOO\}, $\{TST\}, $\{ALL\}}"
+echo {a, b, c}
+## a comment
+echo foo
+
+`
+
+func TestLexAction(t *testing.T) {
+	l := &lexer{input: lexerInputAction, start: 90, pos: 90, tokens: make(chan token.Token)}
+	go lexAction(l)
+	select {
+	case <-time.After(time.Second * 2):
+		t.Fatal("emitting an action was expected but it wasn't")
+	case tok := <-l.tokens:
+		require.Equal(t, token.ACTION, tok.Type)
+		require.Equal(t, actionMatch, tok.Value)
+	}
+}
